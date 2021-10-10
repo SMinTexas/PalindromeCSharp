26c373d303710c45589935aab0434e6779c4d281
Steven Murray <tstevenm@gmail.com>
2021-10-10 14:58:30 -0500

    Add project files.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Palindrome
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string enteredWord, reversedWord;
+            Console.WriteLine("Enter a word ");
+            enteredWord = Console.ReadLine();
+            char[] charArray = enteredWord.ToCharArray();
+            Array.Reverse(charArray);
+            reversedWord = new string(charArray);
+            bool isPalindrome = enteredWord.Equals(reversedWord, StringComparison.OrdinalIgnoreCase);
+            if (isPalindrome)
+            {
+                Console.WriteLine(enteredWord + " is a Palindrome");
+            }
+            else
+            {
+                Console.WriteLine(enteredWord + " is not a Palindrome");
+            }
+            Console.ReadKey();
+        }
+    }
+}
